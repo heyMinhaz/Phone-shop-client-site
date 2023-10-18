3bc7928dd61e78eb7fef3a464d118a06ea164295
Minhazul Abdin <hey_minhaz@Minhazuls-MacBook-Air.local>
2023-10-18 14:07:46 +0600

    form design

--- src/components/pages/Addproducts.jsx
+++ src/components/pages/Addproducts.jsx
@@ -4,9 +4,33 @@
 import React from 'react';
 
 const Addproducts = () => {
+
+    const handelRegister = (e) => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget)
+
+        const name = form.get('name');
+     
+        const brandname = form.get("brandname");
+
+        const Price = form.get("Price");
+        const type = form.get("type");
+        const rating = form.get("rating");
+        const textarea = form.get("textarea");
+        const photourl = form.get("photourl");
+        
+
+        
+
+        console.log(name, Price, brandname, type, rating, textarea, photourl);
+
+    }
+
+
     return (
       <div>
-        <form>
+        <form onSubmit={handelRegister}>
           <section className="bg-white dark:bg-gray-900">
             <div className="flex justify-center min-h-screen">
               <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
@@ -45,26 +69,48 @@ const Addproducts = () => {
                       />
                     </div>
 
-                    <div className=" flex flex-row">
+                    <div className=" lg:flex lg:flex-row">
                       <div>
-                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                        <label className="block lg:mb-2 text-sm text-gray-600 dark:text-gray-200">
                           Price $
                         </label>
                         <input
                           type="text"
+                          name="Price"
                           placeholder="Price"
-                          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                          className="block w-full px-5 py-3 lg:mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                       </div>
 
-                      <div className="mt-7 ml-20">
-                        <select className="select select-info w-full max-w-xs">
+                      <div className="mt-7 lg:ml-20 ">
+                        <select
+                          name="type"
+                          className="select select-info w-full max-w-xs"
+                        >
                           <option disabled selected>
                             Select Type
                           </option>
                           <option>Phone</option>
                           <option>Computer</option>
                         </select>
+                      </div>
+
+                      <div className="lg:mb-5">
+                        <label
+                          for="fName"
+                          className="lg:mb-1 lg:block text-gray-700"
+                        >
+                          <span className="lg:ml-10 text-sm text-gray-700">
+                            Rating
+                          </span>
+                        </label>
+                        <input
+                          type="text"
+                          name="rating"
+                          id="fName"
+                          placeholder="Rating"
+                          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md lg:ml-10"
+                        />
                       </div>
                     </div>
 
@@ -74,6 +120,7 @@ const Addproducts = () => {
                       </label>
                       <input
                         type="text"
+                        name="photourl"
                         placeholder="Photo Url"
                         className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                       />
@@ -94,6 +141,7 @@ const Addproducts = () => {
                       <textarea
                         id="message"
                         rows="4"
+                        name="textarea"
                         className="block w-full px-5 py-3 mt-2  placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                         placeholder="  Product Description..."
                       ></textarea>
